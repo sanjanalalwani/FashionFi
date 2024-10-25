4d834bf0f7374ca0b168bdd71bf161f5516fc6c0
Sanjana <sanjudumy@gmail.com>
2024-10-25 22:42:25 +0530

    init

--- src/pages/Wishlist.jsx
+++ src/pages/Wishlist.jsx
@@ -1,7 +1,9 @@
 import React from "react";
+import { Footer, Navbar } from "../components"; // Ensure the path is correct
 import { useSelector, useDispatch } from "react-redux";
 import { removeWishlist } from "../redux/action";
 import toast from "react-hot-toast";
+import { Link } from "react-router-dom";
 
 const Wishlist = () => {
   const wishlist = useSelector((state) => state.handleWishlist);
@@ -12,31 +14,52 @@ const Wishlist = () => {
     toast.success("Product removed from wishlist!");
   };
 
-  return (
-    <div className="container my-3 py-3">
-      <h2 className="display-5 text-center">Your Wishlist</h2>
-      <hr />
+  const EmptyWishlist = () => {
+    return (
+      <div className="container">
+        <div className="row">
+          <div className="col-md-12 py-5 bg-light text-center">
+            <h4 className="p-3 display-5">Your Wishlist is Empty</h4>
+            <Link to="/" className="btn btn-outline-dark mx-4">
+              <i className="fa fa-arrow-left"></i> Continue Shopping
+            </Link>
+          </div>
+        </div>
+      </div>
+    );
+  };
+
+  const ShowWishlist = () => {
+    return (
       <div className="row">
-        {wishlist.length === 0 ? (
-          <p className="text-center">Your wishlist is empty.</p>
-        ) : (
-          wishlist.map((product) => (
-            <div key={product.id} className="col-md-4 col-sm-6 col-xs-8 col-12 mb-4">
-              <div className="card text-center h-100">
-                <img className="card-img-top p-3" src={product.image} alt={product.title} height={300} />
-                <div className="card-body">
-                  <h5 className="card-title">{product.title}</h5>
-                  <p className="card-text">{product.description}</p>
-                  <button className="btn btn-danger" onClick={() => removeFromWishlist(product.id)}>
-                    Remove from Wishlist
-                  </button>
-                </div>
+        {wishlist.map((product) => (
+          <div key={product.id} className="col-md-4 col-sm-6 col-xs-8 col-12 mb-4">
+            <div className="card text-center h-100">
+              <img className="card-img-top p-3" src={product.image} alt={product.title} height={300} />
+              <div className="card-body">
+                <h5 className="card-title">{product.title}</h5>
+                <p className="card-text">{product.description}</p>
+                <button className="btn btn-danger" onClick={() => removeFromWishlist(product.id)}>
+                  Remove from Wishlist
+                </button>
               </div>
             </div>
-          ))
-        )}
+          </div>
+        ))}
       </div>
-    </div>
+    );
+  };
+
+  return (
+    <>
+      <Navbar />
+      <div className="container my-3 py-3">
+        <h2 className="display-5 text-center">Your Wishlist</h2>
+        <hr />
+        {wishlist.length > 0 ? <ShowWishlist /> : <EmptyWishlist />}
+      </div>
+      <Footer />
+    </>
   );
 };
 
