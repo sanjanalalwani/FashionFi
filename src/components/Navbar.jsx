0742da8a9883cce23d5931a726e2d116244f662a
Sanjana <sanjudumy@gmail.com>
2024-10-25 20:24:42 +0530

    init

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,13 +2,13 @@ import React from 'react'
 import { NavLink } from 'react-router-dom'
 import { useSelector } from 'react-redux'
 import FashionFi from '../assets/FashionFi.png';
-
+import './navbar.css';
 const Navbar = () => {
     const state = useSelector(state => state.handleCart)
     return (
         <nav className="navbar navbar-expand-lg navbar-light bg-light py-3 sticky-top">
             <div className="container">
-                <NavLink className="navbar-brand fw-bold fs-4 px-2" to="/">FashionFi</NavLink>
+                <NavLink className="navbar-brand fw-bold fs-4 px-2" to="/"><img src = {FashionFi}></img>FashionFi</NavLink>
                 <button className="navbar-toggler mx-2" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                     <span className="navbar-toggler-icon"></span>
                 </button>
